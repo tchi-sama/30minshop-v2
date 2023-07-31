fa68a9f7b63f66eb3aaf5396ffc35756932869d7
tchi-sama <tchisama41@gmail.com>
2023-07-31 17:08:44 +0100

    now openai is working

--- back-end/openai/index.js
+++ back-end/openai/index.js
@@ -1,18 +1,29 @@
 
 
 const { Configuration, OpenAIApi } =require("openai");
+require("dotenv").config();
 const configuration = new Configuration({
-    apiKey:"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", 
+    apiKey:process.env.OPENAI_KEY, 
 });
 const openai = new OpenAIApi(configuration);
 
 async function start(){
-    const response = await openai.createCompletion({
-        model:"text-davinci-003",
-        prompt:"a dog joke",
-        temperature:0,
-        max_tokens:100,
+    const response = await openai.createChatCompletion({
+    model:"gpt-3.5-turbo",
+    messages: [
+        {
+            "role": "system",
+            "content": "You will be provided with a infos, and your task is create a javascript object that the object should be like  {name:(name),job:(job),instagram:(instagram),facebook:(facebook)} you will put the infos in there places but if the info is not provided you can set it to null"
+          },
+          {
+            "role": "user",
+            "content": "i'm abdessamad my work is a full stack dev , my instagram is tchisama.life"
+          }
+      ],
+    temperature:0,
+    max_tokens:100,
     });
+    console.log(response.data.choices[0].message.content)
 }
 
 start();
